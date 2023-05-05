30172f2bbb13b86192a45fb2acc2203b3962d87e
Sulaimanma <54430839+Sulaimanma@users.noreply.github.com>
2023-05-05 08:53:06 +0000

    Generated filtered geojson

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import json
 
-geojson_data = """
-{
-"type": "FeatureCollection",
-"name": "Park___Barbeque_locations",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "OBJECTID": 1, "SAP_EQUIPMENT": "200479102", "ITEM_DESCRIPTION": "BBQ - ELECTRIC SINGLE", "ITEM_TYPE": "BARBEQUE", "SAP_FUNCTIONAL_LOCATION": "B-RE-1866-N002", "PARK_NUMBER": "D0287", "PARK_NAME": "SHEPHERD PLACE PARK", "SUBURB": "SHORNCLIFFE", "POWER_TYPE": "MAINS POWER", "NUMBER_OF_PLATES": 1, "LONG": 153.080779807, "LAT": -27.322802682 }, "geometry": { "type": "Point", "coordinates": [ 153.080779807000113, -27.322802681999974 ] } },
-{ "type": "Feature", "properties": { "OBJECTID": 2, "SAP_EQUIPMENT": "100293721", "ITEM_DESCRIPTION": "BBQ - ELECTRIC DOUBLE", "ITEM_TYPE": "BARBEQUE", "SAP_FUNCTIONAL_LOCATION": "B-RE-0169-N003", "PARK_NUMBER": "D2007", "PARK_NAME": "CASCADE DRIVE PARK (NO.20)", "SUBURB": "FOREST LAKE", "POWER_TYPE": "MAINS POWER", "NUMBER_OF_PLATES": 2, "LONG": 152.96464914500001, "LAT": -27.629073494999901 }, "geometry": { "type": "Point", "coordinates": [ 152.964649145000067, -27.629073494999943 ] } }
-]
-}
-
-"""
+# Read the GeoJSON data from the file
+with open('Park_%E2%80%94_Barbeque_locations.geojson', 'r') as file:
+    geojson_data = file.read()
 
 data = json.loads(geojson_data)
 filtered_features = []
 
 for feature in data["features"]:
-  if "ELECTRIC" not in feature["properties"]["ITEM_DESCRIPTION"]:
-    filtered_features.append(feature)
+    if "ELECTRIC" not in feature["properties"]["ITEM_DESCRIPTION"]:
+        filtered_features.append(feature)
 
 data["features"] = filtered_features
-filtered_geojson_data = json.dumps(data)
 
-print(filtered_geojson_data)
+# Save the filtered GeoJSON data to a new file
+with open('filtered_geojson_data.geojson', 'w') as file:
+    json.dump(data, file)
+
+print("Filtered GeoJSON data has been saved to 'filtered_geojson_data.geojson'")
